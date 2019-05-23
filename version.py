bbe2c9f089ffe2562f12611e6032ee2918a2dbc7
pr-supa <psp.11.jb@gmail.com>
2019-05-23 17:55:40 +0530

    t line from no 3 to 9

--- version.py
+++ version.py
@@ -1,16 +1,13 @@
 import sys
 
-# print(sys.version)
-# print(sys.path)
-# name = 'Python'
-# print(f"He is my Friend {name}")
-# for i in sys.path:
-#     print(i)
-# print(sys.executable)
+print(sys.version)
+print(sys.path)
+name = 'Python'
+print(f"He is my Friend {name}")
+for i in sys.path:
+    print(i)
+print(sys.executable)
 
-
-def greet(who_to_greet):
-    return "Hello, {}".format(who_to_greet)
 
 
 print(greet("corey"))
