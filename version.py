9f5c92593c6580d6fcc0915d3472b2a338f17eba
pr-supa <psp.11.jb@gmail.com>
2019-05-23 17:52:46 +0530

    intial commit

---
+++ version.py
@@ -0,0 +1,21 @@
+import sys
+
+# print(sys.version)
+# print(sys.path)
+# name = 'Python'
+# print(f"He is my Friend {name}")
+# for i in sys.path:
+#     print(i)
+# print(sys.executable)
+
+
+def greet(who_to_greet):
+    return "Hello, {}".format(who_to_greet)
+
+
+print(greet("corey"))
+print(greet("parth"))
+print("damn good")
+
+# name = input("Your name?")
+# print("hello,", name)
